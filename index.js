8c379fef6b50f18642468b038e651b2b8830f900
jaspersanchez <jaspersanchez1996@gmail.com>
2024-04-16 15:20:51 +0800

    uibuild

--- index.js
+++ index.js
@@ -99,8 +99,6 @@ app.post('/api/persons', (req, res, next) => {
 })
 
 const errorHandler = (error, req, res, next) => {
-  console.error(error.message)
-
   if (error.name === 'CastError') {
     return res.status(400).send({ error: 'malformatted id' })
   } else if (error.name === 'ValidationError') {
--- models/people.js
+++ models/people.js
@@ -16,7 +16,17 @@ const personSchema = new mongoose.Schema({
     type: String,
     minLength: 3,
   },
-  number: String,
+  number: {
+    type: String,
+    validate: {
+      validator: function (v) {
+        return /\d{2,3}-\d{6,}/.test(v)
+      },
+      message: (props) => {
+        return `${props.value} is not a valid phone number!`
+      },
+    },
+  },
 })
 
 personSchema.set('toJSON', {
