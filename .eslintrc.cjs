36c1f591a10b4ae0b29f36473af1cb35358e9de4
jaspersanchez <jaspersanchez1996@gmail.com>
2024-04-17 10:28:29 +0800

    Exercise 3.22

---
+++ .eslintrc.cjs
@@ -0,0 +1,34 @@
+module.exports = {
+  env: {
+    commonjs: true,
+    es2021: true,
+    node: true,
+  },
+  overrides: [
+    {
+      env: {
+        node: true,
+      },
+      files: ['.eslintrc.{js,cjs}'],
+      parserOptions: {
+        sourceType: 'script',
+      },
+    },
+  ],
+  parserOptions: {
+    ecmaVersion: 'latest',
+  },
+  plugins: ['@stylistic/js'],
+  extends: 'eslint:recommended',
+  rules: {
+    '@stylistic/js/indent': ['error', 2],
+    '@stylistic/js/linebreak-style': ['error', 'unix'],
+    '@stylistic/js/quotes': ['error', 'single'],
+    '@stylistic/js/semi': ['error', 'never'],
+    'no-trailing-spaces': 'error',
+    'object-curly-spacing': ['error', 'always'],
+    'arrow-spacing': ['error', { before: true, after: true }],
+    eqeqeq: 'error',
+    'no-console': 0,
+  },
+}
--- index.js
+++ index.js
@@ -9,8 +9,8 @@ morgan.token('request-body', (req, res) => JSON.stringify(req.body))
 app.use(express.static('dist'))
 app.use(
   morgan(
-    ':method :url :status :res[content-length] - :response-time ms :request-body',
-  ),
+    ':method :url :status :res[content-length] - :response-time ms :request-body'
+  )
 )
 app.use(express.json())
 
